20258ec1af78163a03da1c5a3bc0669a1ba5dd49
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-04-06 21:51:47 +0200

    init navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,18 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Navbar} from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Navbar],
+  template: `
+    <app-navbar></app-navbar>
+    <main class="main">
+      <div class="content">
+        <h1>Hello my friend</h1>
+      </div>
+    </main>
+  `,
 })
 export class App {
   protected readonly title = signal('frontend');
